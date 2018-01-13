5b584b7d7e99e4a631d48f672fb5e0e0f4d7a3a1
Anthony Hung <anthonyhung7@gmail.com>
2018-01-13 17:41:00 +0800

    First commit test

--- NodejsWebApp7/server.js
+++ NodejsWebApp7/server.js
@@ -2,7 +2,16 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	res.send('Hi there!');
+});
+
+app.listen(port, 'localhost');
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
