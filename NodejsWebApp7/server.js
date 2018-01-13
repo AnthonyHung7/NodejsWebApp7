de3072995cd304c0c2656673ebd947bafe0b93ae
Anthony Hung <anthonyhung7@gmail.com>
2018-01-13 22:15:26 +0800

    Blog.Create

--- NodejsWebApp7/server.js
+++ NodejsWebApp7/server.js
@@ -2,9 +2,34 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 
+// APPP CONFIG
+mongoose.connect('mongodb://localhost/restful_blog_app');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: { type: Date, default: Date.now }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+	title: "Test Blog",
+	image: "https://images.unsplash.com/photo-1446231855385-1d4b0f025248",
+	body: "HELLO THIS IS A BLOG POST!"
+});
+
+// RESTFUL ROUTES
 app.get('/', function (req, res) {
 	console.log('Hello World!!!');
 	console.log('Hello World2!!!');
@@ -12,6 +37,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, 'localhost');
+
 
 //http.createServer(function (req, res) {
 //    res.writeHead(200, { 'Content-Type': 'text/plain' });
